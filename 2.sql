60d5179777e901c09b7f9644c0ca91e5bc1697e6
SanyaATU <61184019+SanyaATU@users.noreply.github.com>
2021-07-20 16:24:15 +0500

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,10 +1,2 @@
-SELECT * FROM users;
-SELECT * FROM orders;
-SELECT * FROM storehouses_products;
-SELECT * FROM storehouses;
-SELECT * FROM products;
-SELECT * FROM orders_products;
-SELECT * FROM catalogs;
-SELECT * FROM discounts;
 
 SELECT products.id, products.name, products.catalog_id, catalogs.name FROM products JOIN catalogs ON products.catalog_id = catalogs.id;
